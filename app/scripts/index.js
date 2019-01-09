abf10975ac25510949bd02cc7d75cc3665c665cf
Patrick Mockridge <patrick@engzig.com>
2019-01-09 11:08:31 +0000

    testing on a Mac

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -26,8 +26,21 @@ const createStar = async () => {
 }
 
 // Add a function lookUp to Lookup a star by ID using tokenIdToStarInfo()
+const lookUpStarByTokenId = async () => {
+  const instance = await StarNotary.deployed();
+  const tokenId = parseInt(document.getElementById("tokenId").value);
+  let starNameOf = await instance.lookUpTokenIdToStarInfo.call(tokenId, {from: account});
+  App.setStatusName("Star name for tokenId " + tokenId + " is: " + starNameOf);
+}
 
-//
+// Added function to transfer star from webapp
+const transferStarToken = async() => {
+  const instance = await StarNotary.deployed();
+  const starToBeTransferred = document.getElementById('starToBeTransferred').value;
+  const accountToBeSentTo = document.getElementById('accountToBeSentTo').value;
+  await instance.starTransfer(accountToBeSentTo, starToBeTransferred, {from: account});
+  alert('Star with tokenId ' + starToBeTransferred + ' has been sent to account:\n' + accountToBeSentTo);
+}
 
 const App = {
   start: function () {
--- truffle.js
+++ truffle.js
@@ -4,12 +4,30 @@ require('babel-register')
 // Edit truffle.config file should have settings to deploy the contract to the Rinkeby Public Network.
 // Infura should be used in the truffle.config file for deployment to Rinkeby.
 
+const HDWalletProvider = require('truffle-hdwallet-provider');
+
 module.exports = {
   networks: {
-    ganache: {
+    develop: {
       host: '127.0.0.1',
-      port: 7545,
+      port: 8545,
       network_id: '*' // Match any network id
+    },
+    rinkeby: {
+      provider: function() {
+        return new HDWalletProvider('switch current current usual mutual close awesome broccoli salon coast clutch knee', 'https://rinkeby.infura.io/v3/0c9165c8582f4f3abed7c07313d8ba7e')
+      },
+      network_id: '4',
+      gas: 4500000,
+      gasPrice: 10000000000
+    }
+  },
+  compilers: {
+    solc: {
+      version: './node_modules/solc', // Version is managed in package.json as an NPM dependency.
+			optimizer: {
+				enabled: true,
+			},
     }
   }
 }
